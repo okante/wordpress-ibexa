c550fcc6bb133eaedd8f4e75878ce9a4c58bcf7d
achourhichem <achourhichem3@gmail.com>
2022-09-21 10:39:06 +0200

    fix retours

--- bundle/Service/PageService.php
+++ bundle/Service/PageService.php
@@ -2,91 +2,10 @@
 
 namespace Almaviacx\Bundle\Ibexa\WordPress\Service;
 
-use Almaviacx\Bundle\Ibexa\WordPress\Exceptions\PostNotFoundException;
-use Almaviacx\Bundle\Ibexa\WordPress\ValueObject\Post;
-use Almaviacx\Bundle\Ibexa\WordPress\ValueObject\WPObject;
-use Exception;
-use Ibexa\Contracts\Core\Repository\Exceptions\BadStateException;
-use Ibexa\Contracts\Core\Repository\Exceptions\ContentFieldValidationException;
-use Ibexa\Contracts\Core\Repository\Exceptions\ContentValidationException;
-use Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException;
-use Ibexa\Contracts\Core\Repository\Exceptions\UnauthorizedException;
-use Ibexa\Contracts\Core\Repository\Values\Content\Content;
-use Psr\Cache\CacheException;
-use Psr\Cache\InvalidArgumentException;
-
-final class PageService extends AbstractService
+final class PageService extends PostAbstractService
 {
     public const ROOT = 'pages';
     public const SERVICE_URL = '/pages';
     public const DATATYPE = 'page';
-    private CategoryService $categoryService;
-    private AuthorService $authorService;
-
-
-    protected string $objectClass = Post::class;
-    protected string $exceptionClass = PostNotFoundException::class;
-
-    /**
-     * @required
-     * @param CategoryService $categoryService
-     * @param AuthorService $authorService
-     * @return $this
-     */
-    public function setRelatedServices(CategoryService $categoryService, AuthorService $authorService): self
-    {
-        $this->categoryService = $categoryService;
-        $this->authorService = $authorService;
-        return $this;
-    }
-
-    protected function createObject(array $data): ?WPObject
-    {
-        $data['categoryIds'] = (array)($data['categories']?? []);
-        $data['authorId'] = (int) ($data['author']?? null);
-        unset($data['author'], $data['categories']);
-        return parent::createObject($data);
-    }
-
-    /**
-     * @param WPObject $object
-     * @param $lang
-     * @param bool $update
-     * @return Content|null
-     * @throws BadStateException
-     * @throws ContentFieldValidationException
-     * @throws ContentValidationException
-     * @throws NotFoundException
-     * @throws UnauthorizedException
-     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException
-     */
-    public function  createContent(WPObject $object, $lang = 'eng-GB', bool $update = true): ?Content
-    {
-        if ($object instanceof Post) {
-            $postId = $object->getWPObjectId();
-            $remoteId = self::DATATYPE. '-'. $postId;
-            $values = $this->configResolver->getParameter(self::ROOT, self::NAMESPACE);
-            $parentLocationId = $values['parent_location']??null;
-
-            $authorId = $object->authorId;
-            if (!empty($authorId)) {
-                try {
-                    $object->setAuthorContent($this->authorService->createAsSubObject($authorId)->contentInfo);
-                } catch(Exception $e) {
-                    $this->error(__METHOD__, ['authorId' => $authorId, 'postId' => $postId, 'e' => $e->getTraceAsString()]);
-                }
-            }
-            $categoryId = (int) (array_values($object->categoryIds)[0] ?? null);// array_shift(array_values($array));($object->categoryIds);
-            if (!empty($categoryId)) {
-                try {
-                    $parentLocationId = $this->categoryService->createAsSubObject($categoryId)->contentInfo->mainLocationId;
-                } catch(Exception $e) {
-                    $this->error(__METHOD__, ['category' => $categoryId, 'postId' => $postId, 'e' => $e->getTraceAsString()]);
-                }
-            }
-            return $this->innerCreateContent($object, $values, $remoteId, $parentLocationId, $lang, $update);
-        }
-        return null;
-    }
 
 }
